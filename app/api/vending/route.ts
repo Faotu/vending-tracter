0d498722fc93eaa464005d6dc12bc2fdcafe223c
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-10-16 19:36:05 +0100

    updated

--- app/api/vending/route.ts
+++ app/api/vending/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { z } from "zod";
-
-const createVendingSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-});
+import { createVendingSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
--- app/vending/new/page.tsx
+++ app/vending/new/page.tsx
@@ -2,16 +2,53 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface VendingForm {
+  title: string;
+  description: string;
+}
 
 const NewVendingPage = () => {
+  const router = useRouter();
+
+  const { register, control, handleSubmit } = useForm<VendingForm>();
+  const [error, setError] = useState("");
+
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root>
-        <TextField.Input placeholder="Title" />
-      </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Vending</Button>
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/vending", data);
+            router.push("/vending");
+          } catch (error) {
+            setError("An unexpected error occured!...");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Vending</Button>
+      </form>
     </div>
   );
 };
